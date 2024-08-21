afb1386f83e16ffd44bf7e13febd74cba25a2bff
samuelsilva <samucasilva@gmail.com>
2024-08-21 19:08:05 -0300

    Alteração para também aceitar vírgula nos valores de moeda (R$)

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,24 +5,28 @@ public class ContaTerminal {
         // Criação do objeto Scanner para capturar a entrada do usuário via terminal
         Scanner scanner = new Scanner(System.in);
 
-        // Exibindo as mensagens e capturando as entradas do usuário
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.nextLine();
-
-        System.out.println("Por favor, digite o número da Conta:");
+        System.out.println("Por favor, digite o numero da Conta:");
         int numero = scanner.nextInt();
         scanner.nextLine(); // Consumir a nova linha após o nextInt()
+
+
+        // Exibindo as mensagens e capturando as entradas do usuï¿½rio
+        System.out.println("Por favor, digite o número da Agencia:");
+        String agencia = scanner.nextLine();
+
 
         System.out.println("Por favor, digite o seu nome:");
         String nomeCliente = scanner.nextLine();
 
         System.out.println("Por favor, digite o saldo inicial:");
-        double saldo = scanner.nextDouble();
+        String saldoInput = scanner.nextLine();
+        saldoInput = saldoInput.replace(",", "."); // Substitui vírgula por ponto
+        double saldo = Double.parseDouble(saldoInput);
 
-        // Exibindo a mensagem final com as informações fornecidas
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
-                           "sua agência é " + agencia + ", conta " + numero +
-                           " e seu saldo " + saldo + " já está disponível para saque.");
+        // Exibindo a mensagem final com as informaï¿½ï¿½es fornecidas
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                           "sua agencia " + agencia + ", conta " + numero +
+                           " e seu saldo " + saldo + " ja esta disponivel para saque.");
 
         // Fechar o scanner
         scanner.close();
